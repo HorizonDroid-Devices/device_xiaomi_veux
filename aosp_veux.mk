7b7a49db2993584bbcfdb186d327df1c079009cf
aswinop <itsaswinop@gmail.com>
2024-01-03 23:26:21 +0530

    veux: Welcome to HorizonDroid
    Signed-off-by: aswinop <itsaswinop@gmail.com>

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -12,19 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common Matrixx stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common HorizonDroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Matrixx 
-MATRIXX_BUILD_TYPE := Official
-MATRIXX_MAINTAINER := AswinOP
-MATRIXX_CHIPSET := SM6375
-MATRIXX_BATTERY := 5000mah
-MATRIXX_DISPLAY := 1080X2400
-
-# Gapps
-WITH_GMS := true
-
+# HorizonDroid prop
+CUSTOM_BUILD_TYPE := Official
 
 # AOSP Recovery
 TARGET_USES_AOSP_RECOVERY := true
@@ -32,18 +24,13 @@ TARGET_USES_AOSP_RECOVERY := true
 # BOOT_ANIMATION
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Nuke AudioFX
-TARGET_EXCLUDES_AUDIOFX := true
-
 #Blur effect
 TARGET_ENABLE_BLUR := true	
 
 # Device identifier
-PRODUCT_NAME := lineage_veux
+PRODUCT_NAME := aosp_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X4 Pro 5G
-
-# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
